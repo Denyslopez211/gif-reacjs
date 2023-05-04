bdef012f16566e51255a6ce2d5cac9cb4ceb70b1
Denys Lopez <denyslopez211@gmail.com>
2023-05-04 07:21:29 -0400

    Se agrega el boton para descargar

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,19 +3,31 @@ import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Dragon Ball"]);
+  const [limit, setLimit] = useState(5);
 
-  const onAddCategory = (newCategory) => {
+  const onAddCategory = (newCategory, limit) => {
     if (categories.includes(newCategory)) return;
+    setLimit(limit);
     setCategories([newCategory, ...categories]);
+  };
+  const onRemoveCategory = (deleteCategory) => {
+    setCategories(categories.filter((category) => category !== deleteCategory));
   };
 
   return (
     <>
-      <h1>GifExpertApp</h1>
+      <h1>Buscar Gifs</h1>
 
-      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+      <AddCategory
+        onNewCategory={(event, limit) => onAddCategory(event, limit)}
+      />
       {categories.map((category) => (
-        <GifGrid key={category} category={category} />
+        <GifGrid
+          onRemoveCategory={(event) => onRemoveCategory(event)}
+          key={category}
+          category={category}
+          limit={limit}
+        />
       ))}
     </>
   );
