b0acae12cba902c40986e74fddb63ab48b5abb81
Denys Lopez <denyslopez211@gmail.com>
2023-05-04 09:21:12 -0400

    Cambio de docs a dist

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,9 +1,7 @@
 export const getGifs = async (category, limit) => {
-	const url = `https://api.giphy.com/v1/gifs/search?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&q=${category}&limit=${limit}`;
+	const url = `https://api.giphy.com/v1/stickers/search?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&q=${category}&limit=${limit}`;
 	const resp = await fetch(url);
 	const { data } = await resp.json();
-
-	// console.log(data);
 
 	const gifs = data.map(img => ({
 		id: img.id,
@@ -13,3 +11,9 @@ export const getGifs = async (category, limit) => {
 
 	return gifs;
 };
+
+// const url = `https://api.giphy.com/v1/stickers/trending?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&limit=${limit}`;
+// // const url = `https://api.giphy.com/v1/gifs/search?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&q=${category}&limit=${limit}`;
+// 	const url = `https://api.giphy.com/v1/stickers/search?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&q=${category}&limit=${limit}`;
+// const url = `https://api.giphy.com/v1/stickers/translate?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&s=${category}&limit=${limit}`;
+// const url = `https://api.giphy.com/v2/emoji?api_key=LGCUDOzGk7BJw65fhiKFxQON1gnUCFF5&limit=${limit}`;
